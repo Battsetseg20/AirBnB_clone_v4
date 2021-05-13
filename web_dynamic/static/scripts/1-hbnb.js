0821adf98d0c32e60ca306c395d228f40d3c8848
fourbet <ofourbet@yahoo.fr>
2021-05-13 15:06:44 +0000

    fix(): semistand compliant

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
-  amenities = {};
-  string = '';
+  const amenities = {};
   $('input[type=checkbox]').change(
     function () {
       if ($(this).is(':checked')) {
-	amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
       } else {
-	delete amenities[$(this).attr('data-id')];
-      } 
+        delete amenities[$(this).attr('data-id')];
+      }
       $('.amenities h4').text(Object.values(amenities));
-  });
+    });
 });
