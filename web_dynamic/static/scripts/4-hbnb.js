7bd4b0b48dee80205d72096cda01f1a32af897f3
fourbet <ofourbet@yahoo.fr>
2021-05-15 10:41:00 +0000

    feat(): hbnb

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  const amenities = {};
+  $('input[type=checkbox]').change(
+    function () {
+      if ($(this).is(':checked')) {
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete amenities[$(this).attr('data-id')];
+      }
+      $('.amenities h4').text(Object.values(amenities));
+    });
+  $.get('http://localhost:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    }
+  });
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data, statut) {
+      for (let x = 0; x < data.length; x++) {
+        const place = data[x];
+        $('SECTION.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + ' Guest</div><div class="number_rooms">' + place.number_rooms + ' Bedroom</div><div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom</div></div><div class="user"></div><div class="description">' + place.description + '</div></article>');
+      }
+    }
+  });
+  $(':button').click(function () {
+    const amenitiesId = [];
+    for (const [key, value] of Object.entries(amenities)) {
+      amenitiesId.push(key);
+    }
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      data: JSON.stringify({ amenities: amenitiesId }),
+      success: function (data, statut) {
+        $('SECTION.places').empty();
+        for (let x = 0; x < data.length; x++) {
+          const place = data[x];
+          $('SECTION.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + ' Guest</div><div class="number_rooms">' + place.number_rooms + ' Bedroom</div><div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom</div></div><div class="user"></div><div class="description">' + place.description + '</div></article>');
+        }
+      }
+
+    });
+  });
+});
